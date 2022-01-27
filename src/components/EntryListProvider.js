658f25c15fc904fccfcfc96d8fcd378118bb65ff
Alejandro Segura Espinoza <alex.segura@onibex.com>
2022-01-27 09:54:51 -0600

    se reutilizo el componente de entry list para el componente entry list provider

--- src/components/EntryListProvider.js
+++ src/components/EntryListProvider.js
@@ -4,6 +4,7 @@ import { Context as IncomeTypeContext} from './../context/IncomeTypeContext';
 import { Icon, Input, Button } from 'react-native-elements';
 import tw from 'tailwind-react-native-classnames';
 import IntegerInput from './IntegerInput';
+import EntryList from './EntryList';
 
 const EntryListProvider = ({ data, deleteItem, employeeQty }) => {
 
@@ -27,30 +28,10 @@ const EntryListProvider = ({ data, deleteItem, employeeQty }) => {
             <View style={tw`mt-5`}>
                 {data.map((item) => {
                     return (
-                        <View 
-                            key={item.id}
-                            style={tw``}>
-                            <View 
-                                style={tw`flex-row items-center mb-3 pl-5 pr-5 pt-2 pb-2 bg-gray-200 border border-gray-300`}>
-                                <View style={tw`flex-1`}>
-                                    <Text style={tw`text-black text-lg font-thin`}>{item.name}</Text>
-                                    <View style={tw`flex-row`}>
-                                        <View style={tw`flex-row items-center`}>
-                                            <Text style={tw`text-black text-sm font-thin`}>Placas:</Text>
-                                            <Text style={tw`text-gray-500 text-sm font-thin ml-1`}>{item.car_tag}</Text>
-                                        </View>
-                                        <View style={tw`ml-3 flex-row items-center`}>
-                                            <Text style={tw`text-black text-sm font-thin`}>Modelo:</Text>
-                                            <Text style={tw`text-gray-500 text-sm font-thin ml-1`}>{item.car_model}</Text>
-                                        </View>
-                                    </View>
-                                </View>
-                                <TouchableOpacity 
-                                    key={item.id}
-                                    onPress={() => deleteItem(item.id)}>
-                                    <Icon type='font-awesome' name='trash' size={25} color='red'/>
-                                </TouchableOpacity>
-                            </View>
+                        <View key={item.id}>
+                            <EntryList 
+                                data={[item]}
+                                deleteItem={() => deleteItem(item.id)} />
                             <View style={tw`border-b mb-3 border-gray-300`}>
                                 <Text style={tw`text-black text-xl font-thin pb-3`}>Alta de Empleados</Text>
                                 <Text style={tw`text-black mb-3 text-base font-thin`}>Cantidad de empleados</Text>
